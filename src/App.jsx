881d44aa437724af09c5162b1c1cab92f422ae4f
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-05-29 19:06:10 +0200

    feat: basic main menu

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,33 @@ const StyledLogilabLogo = styled.img`
   top: 70px;
 `
 
+const MainMenu = styled.nav`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  right: 50vw;
+  top: calc(50vh - (var(--lg) * 2.4));
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.a`
+  font-family: "WorkSans";
+  font-weight: 900;
+  text-align: right;
+  font-size: var(--lg);
+  color: var(--blue);
+`;
+
 function App() {
   return (
     <>
       <StyledLogilabLogo src={LogilabLogo} />
+      <MainMenu>
+        <MenuItem>La société</MenuItem>
+        <MenuItem>Nos formations</MenuItem>
+        <MenuItem>Nos clients</MenuItem>
+        <MenuItem>Web sémantique</MenuItem>
+      </MainMenu>
     </>
   )
 }
