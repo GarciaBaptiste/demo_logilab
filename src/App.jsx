4488575035512e3d40b09af166f62cfd3fe9849b
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-05-29 18:43:37 +0200

    feat: add svg logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import react from 'react'
+import styled from 'styled-components'
 import './App.css'
+
+import LogilabLogo from './assets/logo_logilab.svg'
+
+const StyledLogilabLogo = styled.img`
+  position: fixed;
+  width: 185px;
+  left: calc(50vw - 680px);
+  top: 70px;
+`
 
 function App() {
   return (
     <>
+      <StyledLogilabLogo src={LogilabLogo} />
     </>
   )
 }
